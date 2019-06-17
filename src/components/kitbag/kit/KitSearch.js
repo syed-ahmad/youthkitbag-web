16c1d57926db1e890776f0d6a0f37293f64108ab
Phil Moir <pgmoir@gmail.com>
2019-06-17 07:32:08 +0100

    Convert search to redux form

--- src/components/kitbag/kit/KitSearch.js
+++ src/components/kitbag/kit/KitSearch.js
@@ -1,29 +1,47 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchKitBagKits } from '../../../actions/KitbagActions';
 
 class KitSearch extends React.Component {
-  state = { search: '', by: 'all' };
 
-  onFormSubmit = event => {
-    event.preventDefault();
-    this.props.onSubmit(this.state.search, this.state.by) 
+  renderInput(formProps) {
+    return (
+      <input className="form-control" {...formProps.input} {...formProps} />
+    );
+  }
+
+  renderSelect(formProps) {
+    return (
+      <div className="input-group-prepend">
+        <select className="custom-select" {...formProps.input} {...formProps}>
+          <option value="all" >All</option>
+          <option value="title" >Title</option>
+          <option value="activity" >Activity</option>
+          <option value="tag" >Tag</option>
+          <option value="inactive"  >All Inactive</option>
+        </select>
+      </div>
+    );
+  }
+
+  onSubmit = formValues => {
+    const { by, search } = formValues;
+    this.props.actions.fetchKitBagKits(search, by, 1, 24);
   }
 
   render() {
     return (
       <div className="d-inline-block">
-        <form onSubmit={this.onFormSubmit}>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <div className="form-group mr-3">
             <div className="input-group">
-              <div className="input-group-prepend">
-                <select className="custom-select" id="by" name="by" value={this.state.by} onChange={e => this.setState({ by: e.target.value })}>
-                  <option value="all" >All</option>
-                  <option value="title" >Title</option>
-                  <option value="activity" >Activity</option>
-                  <option value="tag" >Tag</option>
-                  <option value="inactive"  >All Inactive</option>
-                </select>
-              </div>
-              <input type="text" className="form-control" aria-label="Search by text" id="search" name="search" value={this.state.value} onChange={e => this.setState({ search: e.target.value })} />
+              <Field name="by" component={this.renderSelect} id="by" />
+              <Field name="search" component={this.renderInput} title="Email"
+                type="text" 
+                id="search" 
+                arialabel="Search by text" />
               <div className="input-group-append">
                 <button className="btn btn-outline-primary" type="submit" id="search">Search</button>
                 <a href="/kitbag/kit/all" className="btn btn-outline-secondary">Clear</a>
@@ -36,4 +54,15 @@ class KitSearch extends React.Component {
   }
 }
 
-export default KitSearch;
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators({ fetchKitBagKits }, dispatch)
+})
+
+KitSearch = connect(mapStateToProps, mapDispatchToProps)(KitSearch);
+
+export default reduxForm({
+  form: 'kitsearch'
+})(KitSearch);
