045536f631e57a3881dac166c72f00b3583864f6
Phil Moir <pgmoir@gmail.com>
2019-06-17 07:35:58 +0100

    Change the name of property wher user gets stored in local storage

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,6 +1,6 @@
 import * as types from './types'
 
 export const getUser = () => async (dispatch) => {
-  const user = JSON.parse(localStorage.getItem('authentication'))
+  const user = JSON.parse(localStorage.getItem('user'))
   dispatch({ type: types.GET_USER, payload: user })
 }
