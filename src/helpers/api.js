a487b75952bcc9f0474e2a840bd3d124b2c3584d
Phil Moir <pgmoir@gmail.com>
2019-06-21 13:39:42 +0100

    Remove api helper for the moment

--- src/helpers/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: process.env.YKBAPI || 'http://localhost:8080',
-  headers: {
-    Authorization: `bearer ${localStorage.getItem('token')}`,
-    'content-type': 'application/json',
-  }
-});
