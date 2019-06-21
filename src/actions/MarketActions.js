87cf1ce338442403f6fd848460a11057f55aca3a
Phil Moir <pgmoir@gmail.com>
2019-06-21 13:38:25 +0100

    Commented out until needed

--- src/actions/MarketActions.js
+++ src/actions/MarketActions.js
@@ -1,43 +1,43 @@
-import API from '../helpers/api'
+// import API from '../helpers/api'
 
-export const fetchMarketForSales = (search = '', by = 'all', page = 1, pagesize = 24) =>  async dispatch => {
-  const response = await API.get('/market/forsale', {
-    params: { search, by, page, pagesize }
-  });
+// export const fetchMarketForSales = (search = '', by = 'all', page = 1, pagesize = 24) =>  async dispatch => {
+//   const response = await API.get('/market/forsale', {
+//     params: { search, by, page, pagesize }
+//   });
 
-  dispatch({ type: 'FETCH_MARKET_FORSALES', payload: response.data });
-};
+//   dispatch({ type: 'FETCH_MARKET_FORSALES', payload: response.data });
+// };
 
-export const fetchMarketForSale = (id) =>  async dispatch => {
-  const response = await API.get(`/market/forsale/${id}`);
+// export const fetchMarketForSale = (id) =>  async dispatch => {
+//   const response = await API.get(`/market/forsale/${id}`);
 
-  dispatch({ type: 'FETCH_MARKET_FORSALE', payload: response.data });
-};
+//   dispatch({ type: 'FETCH_MARKET_FORSALE', payload: response.data });
+// };
 
-export const fetchMarketWanteds = (search = '', by = 'all', page = 1, pagesize = 24) =>  async dispatch => {
-  const response = await API.get('/market/wanted', {
-    params: { search, by, page, pagesize }
-  });
+// export const fetchMarketWanteds = (search = '', by = 'all', page = 1, pagesize = 24) =>  async dispatch => {
+//   const response = await API.get('/market/wanted', {
+//     params: { search, by, page, pagesize }
+//   });
 
-  dispatch({ type: 'FETCH_MARKET_FORSALES', payload: response.data });
-};
+//   dispatch({ type: 'FETCH_MARKET_FORSALES', payload: response.data });
+// };
 
-export const fetchMarketWanted = (id) =>  async dispatch => {
-  const response = await API.get(`/market/wanted/${id}`);
+// export const fetchMarketWanted = (id) =>  async dispatch => {
+//   const response = await API.get(`/market/wanted/${id}`);
 
-  dispatch({ type: 'FETCH_MARKET_FORSALE', payload: response.data });
-};
+//   dispatch({ type: 'FETCH_MARKET_FORSALE', payload: response.data });
+// };
 
-export const fetchMarketStolens = (search = '', by = 'all', page = 1, pagesize = 24) =>  async dispatch => {
-  const response = await API.get('/market/stolen', {
-    params: { search, by, page, pagesize }
-  });
+// export const fetchMarketStolens = (search = '', by = 'all', page = 1, pagesize = 24) =>  async dispatch => {
+//   const response = await API.get('/market/stolen', {
+//     params: { search, by, page, pagesize }
+//   });
 
-  dispatch({ type: 'FETCH_MARKET_FORSALES', payload: response.data });
-};
+//   dispatch({ type: 'FETCH_MARKET_FORSALES', payload: response.data });
+// };
 
-export const fetchMarketStolen = (id) =>  async dispatch => {
-  const response = await API.get(`/market/stolen/${id}`);
+// export const fetchMarketStolen = (id) =>  async dispatch => {
+//   const response = await API.get(`/market/stolen/${id}`);
 
-  dispatch({ type: 'FETCH_MARKET_FORSALE', payload: response.data });
-};
+//   dispatch({ type: 'FETCH_MARKET_FORSALE', payload: response.data });
+// };
