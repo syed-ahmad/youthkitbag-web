85f24e9ac9abe529455a2884b11a14dd13d91ab0
Phil Moir <pgmoir@gmail.com>
2019-06-26 15:02:24 +0100

    Revert app file to just render process variable

--- src/components/App.js
+++ src/components/App.js
@@ -23,42 +23,52 @@ import KitCreate from './kitbag/kit/KitCreate';
 import KitEdit from './kitbag/kit/KitEdit';
 import KitDelete from './kitbag/kit/KitDelete';
 
-class App extends React.Component {
+// class App extends React.Component {
 
+//   render() {
+//     return (
+//       <div className="react-body">
+//         <Router history={history}>
+//           <User />
+//           <Header />
+//           <main>
+//             <noscript>You need to enable JavaScript to run this app.</noscript>
+//             <Link id="main-acc-jump" className="sr-only sr-only-focusable" to="#footer-acc-jump">
+//               <div className="container">
+//                 <span className="skiplink-text">Skip to footer content</span>
+//               </div>
+//             </Link>
+//             <Route path="/" exact component={Home} />
+//             <Route path="/features" exact component={Features} />
+//             <Route path="/pricing" exact component={Pricing} />
+            
+//             <Route path="/auth/signup" exact component={SignUp} />
+//             <Route path="/auth/login" exact component={Login} />
+//             <Route path="/auth/reset" exact component={Reset} />
+//             <Route path="/auth/newpassword" exact component={NewPassword} />
+//             <Route path="/auth/logout" exact component={Logout} />
+
+//             <Route path="/purchase" exact component={Purchase} />
+
+//             <Route path="/kitbag/kits" exact component={KitBag} />
+//             <Route path="/kitbag/kits/new" exact component={KitCreate} />
+//             <Route path="/kitbag/kits/edit/:id" exact component={KitEdit} />
+//             <Route path="/kitbag/kits/delete/:id" exact component={KitDelete} />
+
+//           </main>
+//           <Footer />
+//         </Router>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+class App extends React.Component {
   render() {
     return (
-      <div className="react-body">
-        <Router history={history}>
-          <User />
-          <Header />
-          <main>
-            <noscript>You need to enable JavaScript to run this app.</noscript>
-            <Link id="main-acc-jump" className="sr-only sr-only-focusable" to="#footer-acc-jump">
-              <div className="container">
-                <span className="skiplink-text">Skip to footer content</span>
-              </div>
-            </Link>
-            <Route path="/" exact component={Home} />
-            <Route path="/features" exact component={Features} />
-            <Route path="/pricing" exact component={Pricing} />
-            
-            <Route path="/auth/signup" exact component={SignUp} />
-            <Route path="/auth/login" exact component={Login} />
-            <Route path="/auth/reset" exact component={Reset} />
-            <Route path="/auth/newpassword" exact component={NewPassword} />
-            <Route path="/auth/logout" exact component={Logout} />
-
-            <Route path="/purchase" exact component={Purchase} />
-
-            <Route path="/kitbag/kits" exact component={KitBag} />
-            <Route path="/kitbag/kits/new" exact component={KitCreate} />
-            <Route path="/kitbag/kits/edit/:id" exact component={KitEdit} />
-            <Route path="/kitbag/kits/delete/:id" exact component={KitDelete} />
-
-          </main>
-          <Footer />
-        </Router>
-      </div>
+      <code>Runtime env var example: { process.env.REACT_APP_HELLO }</code>
     );
   }
 }
