b5d1157230cc598f70e5d83d2cd1775a4b2728f7
Phil Moir <pgmoir@gmail.com>
2019-07-03 16:04:52 +0100

    Plain vanilla react form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,47 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as AuthActions from '../../actions';
-import classNames from 'classnames';
+import { login } from '../../actions';
 
 import Title from '../includes/Title';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: '', password: ''};
 
-  renderInput({ input, meta, label, type, autoComplete }) {
-    const inputClasses = classNames({
-      'form-control': true,
-      'is-invalid': meta.touched && meta.invalid
-    });
-
-    return (
-      <div className="form-group">
-        <label htmlFor={input.name}>{label}</label>
-        <input type={type} className={inputClasses} id={input.name} aria-describedby={input.name} {...input} autoComplete={autoComplete} />
-        <div className="invalid-feedback">{meta.error}</div>
-      </div>
-    );
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  renderAlert = () => {
-    console.log(this.props.auth);
-    if (!this.props.auth.loginFailed || !this.props.auth.loginError)
-      return null;
-
-    return (
-      <div className="alert alert-danger" role="alert">
-        {this.props.auth.loginError.data.message}
-      </div>
-    );
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
   }
 
-  onSubmit = (formValues) => {
-    const { email, password } = formValues;
-    this.props.actions.login(email, password);
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.login(this.state.email, this.state.password);
   }
-
+ 
   render() {
     return (
       <div>
@@ -51,10 +32,18 @@ class Login extends React.Component {
             <p className="lead">If you don't have an account already, <Link to="/auth/signup">then sign up for an account</Link>. Or for the forgetful, <Link to="/auth/reset">then reset your password</Link>.</p>
             <div className="row">
               <div className="col-12 col-md-6 mb-3 mx-auto">
-                {this.renderAlert()}
-                <form className="w-100 d-block" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-                  <Field name="email" component={this.renderInput} label="Email" type="email" autoComplete="username email" />
-                  <Field name="password" component={this.renderInput} label="Password" type="password" autoComplete="current-password" />
+                {/* {this.renderAlert()} */}
+                <form className="w-100 d-block" onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input name="email" type="email" className="form-control" aria-describedby="email" autoComplete="username email" value={this.state.email} onChange={this.handleChange} />
+                    <div className="invalid-feedback"></div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input name="password" type="password" className="form-control" aria-describedby="password" autoComplete="current-password" value={this.state.password} onChange={this.handleChange} />
+                    <div className="invalid-feedback"></div>
+                  </div>
                   <button className="btn btn-primary" type="submit">Login</button>
                 </form>
               </div>
@@ -66,31 +55,22 @@ class Login extends React.Component {
   }
 }
 
-const validate = formValues => {
-  const errors = {};
+// const validate = formValues => {
+//   const errors = {};
 
-  if (!formValues.email) {
-    errors.email = 'Please enter a valid email';
-  }
+//   if (!formValues.email) {
+//     errors.email = 'Please enter a valid email';
+//   }
 
-  if (!formValues.password) {
-    errors.password = 'Please enter a valid password';
-  }
+//   if (!formValues.password) {
+//     errors.password = 'Please enter a valid password';
+//   }
 
-  return errors;
-}
+//   return errors;
+// }
 
 const mapStateToProps = state => {
   return { auth: state.auth };
 }
 
-const mapDispatchToProps = dispatch => {
-  return { actions: bindActionCreators(AuthActions, dispatch) };
-}
-
-Login = connect(mapStateToProps, mapDispatchToProps)(Login);
-
-export default reduxForm({
-  form: 'login',
-  validate
-})(Login);
+export default connect(mapStateToProps, { login })(Login);
