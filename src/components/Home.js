8f58131276049c296e5fabab680ed30c17345696
Phil Moir <pgmoir@gmail.com>
2019-09-26 19:49:20 +0100

    Add fontawesome files directly

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,30 @@ class Home extends React.Component {
     return (
       <div>
         <Title title="Welcome to YouthKitbag" />
+      
+
+      <h1>
+                <i class="fas fa-trash-alt    "></i>
+        <i class="fas fa-star    "></i>
+        <i class="fas fa-undo    "></i>
+        <i class="fas fa-check-circle    "></i>
+        <i class="fas fa-times-circle    "></i>
+        <i class="fas fa-question-circle    "></i>
+        <i class="fas fa-meh    "></i>
+        <i class="fas fa-laugh    "></i>
+        <i class="fas fa-sad-tear    "></i>
+        <i class="fas fa-meh-blank    "></i>
+        <i class="fas fa-home    "></i>
+        <i class="fas fa-envelope    "></i>
+        <i class="fas fa-phone    "></i>
+        <i class="fas fa-suitcase    "></i>
+        <i class="fas fa-sign-in-alt    "></i>
+        <i class="fas fa-sign-out-alt    "></i>
+        <i class="fas fa-coffee    "></i>
+        <i class="fas fa-beer    "></i>
+        <i class="fas fa-cocktail    "></i>
+        </h1>
+
       </div>
     )
   }
