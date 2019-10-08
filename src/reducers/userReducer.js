1b2775ab5fda6fcf34622cf695475631e6dd8063
Phil Moir <pgmoir@gmail.com>
2019-10-08 07:31:35 +0100

    No state to update after editing profile. Use refresh user after action

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { GET_USER, LOGOUT, EDIT_USER_PROFILE } from '../actions/types';
+import { GET_USER, LOGOUT } from '../actions/types';
 
 const initialState = { profile: {} };
 
@@ -6,8 +6,6 @@ export default function authentication(state = initialState, action) {
   switch (action.type) {
     case GET_USER:
       return { ...action.payload };
-    case EDIT_USER_PROFILE:
-      return { ...state, profile: action.payload.profile };
     case LOGOUT:
       return initialState;
     default:
