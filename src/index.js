bdc0bae658f3ba626a125d36f01bdc4be611cc98
Phil Moir <pgmoir@gmail.com>
2019-10-10 13:18:44 +0100

    Remove console log of NODE_ENV

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-console.log('NODEENV', process.env.NODE_ENV);
 const composeEnhancers =
   process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
