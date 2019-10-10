653e55621ebeebedd7f9578bbe32ead12c9140e5
Phil Moir <pgmoir@gmail.com>
2019-10-10 13:11:33 +0100

    Switch off development redux tools when not dev

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+console.log('NODEENV', process.env.NODE_ENV);
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
